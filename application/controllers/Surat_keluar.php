6dfd994f9ccd18c14ad030626a5bfcf98f33403f
Sofyaninsomnia <ngodingsekarepe@gmail.com>
2025-07-01 08:24:17 +0700

    test

--- application/controllers/Surat_keluar.php
+++ application/controllers/Surat_keluar.php
@@ -8,7 +8,7 @@ class Surat_keluar extends CI_Controller
         $this->load->model('SK_model');
         $this->load->library('form_validation');
     }
-
+    
     public function index()
     {
         $data['sk'] = $this->SK_model->get();
@@ -21,5 +21,64 @@ class Surat_keluar extends CI_Controller
     public function tambah()
     {
         $this->form_validation->set_rules('judul');
+        $this->form_validation->set_rules('deskripsi');
+        $this->form_validation->set_rules('pengirim');
+        $this->form_validation->set_rules('tujuan');
+        $this->form_validation->set_rules('tanggal');
+
+        if($this->form_validation->run() == FALSE){
+            $this->index();
+        } else {
+            $data = array(
+                'judul' => $this->input->post('judul'),
+                'deskripsi' => $this->input->post('deskripsi'),
+                'pengirim' => $this->input->post('pengirim'),
+                'tujuan' => $this->input->post('tujuan'),
+                'tanggal' => $this->input->post('tanggal')
+            );
+            $this->SK_model->insert($data);
+            redirect('surat_keluar');
+        }
+    }
+
+    public function detail($id) {
+        $data['sk'] = $this->SK_model->get_by_id($id);
+        if (!$data['sk']) {
+            show_404();
+        }
+        $this->load->view('surat/detail', $data);
+    }
+
+    public function edit($id) {
+        $data['sk'] = $this->SK_model->get_by_id($id);
+        $this->load->view('surat/edit', $data);
+    }
+
+    public function update() {
+        $id = $this->input->post('id');
+        $this->form_validation->set_rules('judul');
+        $this->form_validation->set_rules('deskripsi');
+        $this->form_validation->set_rules('pengirim');
+        $this->form_validation->set_rules('tujuan');
+        $this->form_validation->set_rules('tanggal');
+
+        if($this->form_validation->run() == FALSE){
+            $this->edit($id);
+        } else {
+            $data = array(
+                'judul' => $this->input->post('judul'),
+                'deskripsi' => $this->input->post('deskripsi'),
+                'pengirim' => $this->input->post('pengirim'),
+                'tujuan' => $this->input->post('tujuan'),
+                'tanggal' => $this->input->post('tanggal')
+            );
+            $this->SK_model->update($id, $data);
+            redirect('surat_keluar');
+        }
+    }
+
+    public function hapus($id) {
+        $this->SK_model->delete($id);
+        redirect('surat_keluar');
     }
 }
